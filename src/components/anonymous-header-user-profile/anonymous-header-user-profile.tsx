9e7a7d0ce7fdaa939e267c692f2e1f1a11deb260
olga.vokhmintseva <olga.vokhmintseva@travelline.ru>
2025-12-11 11:51:28 +0300

    module9-task3. Верстка приведена в соответствие с макетом для прохождения e2e тестов.

--- src/components/anonymous-header-user-profile/anonymous-header-user-profile.tsx
+++ src/components/anonymous-header-user-profile/anonymous-header-user-profile.tsx
@@ -4,8 +4,10 @@ import {AppRoute} from '../../Const';
 function AnonymousHeaderUserProfile(): JSX.Element {
   return (
     <ul className="header__nav-list">
-      <li className="header__nav-item">
-        <Link className="header__nav-link" to={AppRoute.Login}>
+      <li className="header__nav-item user">
+        <Link className="header__nav-link header__nav-link--profile" to={AppRoute.Login}>
+          <div className="header__avatar-wrapper user__avatar-wrapper">
+          </div>
           <span className="header__login">Sign in</span>
         </Link>
       </li>
