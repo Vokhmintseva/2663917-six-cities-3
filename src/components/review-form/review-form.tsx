7b4a491355b1418f7bcdecc94d9093fc19cb2291
olga.vokhmintseva <olga.vokhmintseva@travelline.ru>
2026-01-29 18:08:08 +0300

    Функции-обработчики событий названы через handle. В login-form.tsx удален избыточный метод loginFormFieldChangeHandle - логин и пароль не сохраняются в состоянии компонента, а достаются из evt при логинаации.

--- src/components/review-form/review-form.tsx
+++ src/components/review-form/review-form.tsx
@@ -17,12 +17,12 @@ function ReviewForm({offerId}: ReviewFormProps): JSX.Element {
   const dispatch = useAppDispatch();
   const isCommentSaving = useAppSelector(getIsCommentSaving);
 
-  const reviewFormFieldChangeHandle = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+  const handleReviewFormFieldChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     const {name, value} = event.target;
     setReviewFormData({...reviewFormData, [name]: name === 'rating' ? Number(value) : value});
   };
 
-  const saveComment = async (evt: FormEvent<HTMLFormElement>) => {
+  const handleFormSubmit = async (evt: FormEvent<HTMLFormElement>) => {
     evt.preventDefault();
     const result = await dispatch(saveCommentAction({
       offerId: offerId,
@@ -43,45 +43,45 @@ function ReviewForm({offerId}: ReviewFormProps): JSX.Element {
   const isFormDisabled = isCommentSaving;
 
   return (
-    <form className="reviews__form form" action="#" method="post" onSubmit={(evt) => void saveComment(evt)}>
+    <form className="reviews__form form" action="#" method="post" onSubmit={(evt) => void handleFormSubmit(evt)}>
       <label className="reviews__label form__label" htmlFor="review">Your review</label>
       <div className="reviews__rating-form form__rating">
-        <input className="form__rating-input visually-hidden" name="rating" value="5" id="5-stars" type="radio" onChange={reviewFormFieldChangeHandle} disabled={isFormDisabled} checked={reviewFormData.rating === 5} />
+        <input className="form__rating-input visually-hidden" name="rating" value="5" id="5-stars" type="radio" onChange={handleReviewFormFieldChange} disabled={isFormDisabled} checked={reviewFormData.rating === 5} />
         <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
           <svg className="form__star-image" width="37" height="33">
             <use xlinkHref="#icon-star"></use>
           </svg>
         </label>
 
-        <input className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars" type="radio" onChange={reviewFormFieldChangeHandle} disabled={isFormDisabled} checked={reviewFormData.rating === 4} />
+        <input className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars" type="radio" onChange={handleReviewFormFieldChange} disabled={isFormDisabled} checked={reviewFormData.rating === 4} />
         <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
           <svg className="form__star-image" width="37" height="33">
             <use xlinkHref="#icon-star"></use>
           </svg>
         </label>
 
-        <input className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars" type="radio" onChange={reviewFormFieldChangeHandle} disabled={isFormDisabled} checked={reviewFormData.rating === 3} />
+        <input className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars" type="radio" onChange={handleReviewFormFieldChange} disabled={isFormDisabled} checked={reviewFormData.rating === 3} />
         <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
           <svg className="form__star-image" width="37" height="33">
             <use xlinkHref="#icon-star"></use>
           </svg>
         </label>
 
-        <input className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars" type="radio" onChange={reviewFormFieldChangeHandle} disabled={isFormDisabled} checked={reviewFormData.rating === 2} />
+        <input className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars" type="radio" onChange={handleReviewFormFieldChange} disabled={isFormDisabled} checked={reviewFormData.rating === 2} />
         <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
           <svg className="form__star-image" width="37" height="33">
             <use xlinkHref="#icon-star"></use>
           </svg>
         </label>
 
-        <input className="form__rating-input visually-hidden" name="rating" value="1" id="1-stars" type="radio" onChange={reviewFormFieldChangeHandle} disabled={isFormDisabled} checked={reviewFormData.rating === 1} />
+        <input className="form__rating-input visually-hidden" name="rating" value="1" id="1-stars" type="radio" onChange={handleReviewFormFieldChange} disabled={isFormDisabled} checked={reviewFormData.rating === 1} />
         <label htmlFor="1-stars" className="reviews__rating-label form__rating-label" title="terribly">
           <svg className="form__star-image" width="37" height="33">
             <use xlinkHref="#icon-star"></use>
           </svg>
         </label>
       </div>
-      <textarea className="reviews__textarea form__textarea" id="review" name="review" value={reviewFormData.review} placeholder="Tell how was your stay, what you like and what can be improved" onChange={reviewFormFieldChangeHandle} disabled={isFormDisabled} />
+      <textarea className="reviews__textarea form__textarea" id="review" name="review" value={reviewFormData.review} placeholder="Tell how was your stay, what you like and what can be improved" onChange={handleReviewFormFieldChange} disabled={isFormDisabled} />
       <div className="reviews__button-wrapper">
         <p className="reviews__help">
           To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">{minReviewLength} characters</b>.
